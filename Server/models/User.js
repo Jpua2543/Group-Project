4dc59654bb121abde883a586a12be239606dda97
John <johnpua@outlook.com>
2023-12-18 08:54:35 -0800

    model first user set up

--- Server/models/User.js
+++ Server/models/User.js
@@ -5,13 +5,26 @@ const User = sequelize.define('User', {
     name: {
         type: DataTypes.STRING,
         allowNull: false
+    },
+    alias: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
     }
     },
     {
     timestamps: true,
-    createdAt: 'created_at', 
+    createdAt: 'created_at',
     updatedAt: 'updated_at',
-    tableName: 'users'
+    tableName: 'users' 
     });
     
     module.exports = User;
