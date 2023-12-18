139e98c8100d564b28d45d0abe9a01fefaf3ad3d
John <johnpua@outlook.com>
2023-12-18 07:12:47 -0800

    Adding name

--- Server/models/User.js
+++ Server/models/User.js
@@ -1,2 +1,18 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/sequelize.config');
+
+const User = sequelize.define('User', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+    },
+    {
+    timestamps: true,
+    createdAt: 'created_at', 
+    updatedAt: 'updated_at',
+    tableName: 'users'
+    });
+    
+    module.exports = User;
+    
